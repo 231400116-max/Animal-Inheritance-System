8e8f1929e65bfb733617d023151a520a947dc2f7
231400116 <231400116@labs.gift.edu.pk>
2026-01-05 15:29:08 +0500

    Meaningful commit message

---
+++ Animal.java
@@ -0,0 +1,24 @@
+public class Bird {
+    private String name;
+    private String color;
+    private int age;
+
+    public Bird(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public
